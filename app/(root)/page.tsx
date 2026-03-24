c32380c2a5245166850898dc9de35c0af487775f
faisal-din <dahrifaisal56@gmail.com>
2026-03-24 20:07:49 +0500

    feat: Homepage layout added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,22 @@
+import PodcastCard from '@/components/PodcastCard';
+import { podcastData } from '@/constants';
+
 export default function Home() {
   return (
     <div className='mt-9 flex flex-col gap-9 md:overflow-hidden'>
       <section className='flex flex-col gap-5'>
         <h1 className='text-20 font-bold '>Trending Podcasts</h1>
+        <div className='podcast_grid'>
+          {podcastData.map(({ id, imgURL, title, description }) => (
+            <PodcastCard
+              key={id}
+              imgUrl={imgURL}
+              title={title}
+              description={description}
+              podcastId={id}
+            />
+          ))}
+        </div>
       </section>
     </div>
   );
