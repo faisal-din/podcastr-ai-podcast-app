0cc0de24a8c498b91adc1bbcbdd978c70ddd7b31
faisal-din <dahrifaisal56@gmail.com>
2026-03-31 14:39:13 +0500

    feat: implement search functionality with debouncing in Discover page

--- app/(root)/discover/page.tsx
+++ app/(root)/discover/page.tsx
@@ -1,7 +1,55 @@
-import React from 'react';
+'use client';
+
+import EmptyState from '@/components/EmptyState';
+import LoaderSpinner from '@/components/LoaderSpinner';
+import PodcastCard from '@/components/PodcastCard';
+import Searchbar from '@/components/Searchbar';
+import { useSearchParams } from 'next/navigation';
+import { useQuery } from 'convex/react';
+import { api } from '@/convex/_generated/api';
 
 const Discover = () => {
-  return <div>Discover</div>;
+  const searchParams = useSearchParams();
+  const search = searchParams.get('search') || '';
+
+  const podcastsData = useQuery(api.podcasts.getPodcastBySearch, {
+    search,
+  });
+
+  return (
+    <div className='flex flex-col gap-9'>
+      <Searchbar />
+      <div className='flex flex-col gap-9'>
+        <h1 className='text-20 font-bold text-white-1'>
+          {!search ? 'Discover Trending Podcasts' : 'Search results for '}
+          {search && <span className='text-white-2'>{search}</span>}
+        </h1>
+        {podcastsData ? (
+          <>
+            {podcastsData.length > 0 ? (
+              <div className='podcast_grid'>
+                {podcastsData?.map(
+                  ({ _id, podcastTitle, podcastDescription, imageUrl }) => (
+                    <PodcastCard
+                      key={_id}
+                      imgUrl={imageUrl!}
+                      title={podcastTitle}
+                      description={podcastDescription}
+                      podcastId={_id}
+                    />
+                  ),
+                )}
+              </div>
+            ) : (
+              <EmptyState title='No results found' />
+            )}
+          </>
+        ) : (
+          <LoaderSpinner />
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Discover;
